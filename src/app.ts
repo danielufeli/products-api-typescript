0c845bb81a443f1e05b51bfb899171e1fdb2d0b7
Daniel Ufeli <danielufeli@yahoo.com>
2022-02-18 10:34:22 +0100

    init push

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import config from 'config';
+import connect from './utils/connect';
+
+const port = config.get<number>('port');
+
+const app = express();
+
+app.listen(port, async () => {
+    console.log('App is Running');
+    await connect();
+})

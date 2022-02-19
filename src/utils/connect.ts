0219437a34a961bb4c7aa53c1f1d6bcc989f0644
danielufeli <danielufeli@yahoo.com>
2022-02-19 05:47:21 +0100

    setup validation, routes

--- src/utils/connect.ts
+++ src/utils/connect.ts
@@ -1,14 +1,15 @@
 import mongoose from 'mongoose';
 import config from 'config';
+import logger from './logger';
 
 const connect = async () => {
     const dbUri = config.get<string>('dbUri');
 
     try {
         await mongoose.connect(dbUri);
-        console.log("DB Connected");
+        logger.info("DB Connected");
     } catch (error) {
-        console.error('Could not connect to db');
+        logger.error('Could not connect to db');
         process.exit(1);
     }
 }

f1e9cb30cc2305a7b90b204305371a82b5a60971
danielufeli <danielufeli@yahoo.com>
2022-02-21 10:34:23 +0100

    create user-functionality

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,8 @@ const port = config.get<number>('port');
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(port, async () => {
   logger.info(`App is Running at http://localhost:${port}`);
   await connect();
